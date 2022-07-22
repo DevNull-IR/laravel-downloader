f48b579e7941aa8103d11c2468122ed7bb1fe8fe
Ali Dev <97373457+DevNull-IR@users.noreply.github.com>
2022-07-22 19:50:54 +0430

    change download view

--- src/resources/views/download.blade.php
+++ src/resources/views/download.blade.php
@@ -1,7 +1,24 @@
-<h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="refresh" content="0; url={{$redirect}}">
+    <link href="http://fonts.cdnfonts.com/css/thank-you" rel="stylesheet">
+    <title>Thank you for download</title>
+    <style>
+        *{
+            font-family: 'Thank You', sans-serif!important;
+        }
+    </style>
+</head>
+<body>
+<h1 style="text-align: center;">
     ThankYou For You
 </h1>
-<script>
-    window.open("{{$redirect}}", "_self");
-</script>
+</body>
+</html>
+
 
